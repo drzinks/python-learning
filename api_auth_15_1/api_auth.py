c7a1bd16ac9528388961447e7b0ed6b4173fc36a
drzinks <you@example.com>
2024-10-21 13:01:50 +0200

    json parse cont

--- api_auth_15_1/api_auth.py
+++ api_auth_15_1/api_auth.py
@@ -3,7 +3,6 @@ import json
 import webbrowser
 from pprint import pprint
 
-#x1uxtOdnPNVh7CCuSIpoYg35NIxUIUNB
 params = {
     "api_key" : "x1uxtOdnPNVh7CCuSIpoYg35NIxUIUNB",
     "country" : "pl",
@@ -11,5 +10,8 @@ params = {
 }
 
 site = requests.get("https://calendarific.com/api/v2/holidays",params)
-holidays = site.json()
-pprint(holidays)
+holidays = site.json()["response"]["holidays"]
+for holiday in holidays:
+    if(holiday["type"][0] == 'National holiday'):
+        pprint(holiday["date"]["iso"])
+        pprint(holiday["name"])
