bb802490c1f9e5a0df9c43de2fd01a870723546f
drzinks <you@example.com>
2024-10-25 14:52:14 +0200

    generator functions explained

--- generating_functions_17/generating_functions_explained.py
+++ generating_functions_17/generating_functions_explained.py
@@ -1,16 +1,19 @@
 def generator():
-    x = 0
+    x = 1
     while True:
-        x = x + 1
-        print(f"1. Przed yield, x = {x}")
-        x = yield x * x
-        print(f"2. Po yield, x = {x}")
+        sent_value = yield x * x  # Tutaj x może stać się None
+        x = sent_value if sent_value is not None else x + 1  # Używamy wartości z send() lub zwiększamy x
 
 g = generator()
+
 numbers = []
-for _ in range(1,10):
-    numbers.extend(next(g))
+for _ in range(1, 10):
+    numbers.append(next(g))
 print(numbers)
+
 wynik = g.send(15)
 wynik2 = next(g)
+wynik3 = g.send(5)
 print(wynik)
+print(wynik2)
+print(wynik3)
